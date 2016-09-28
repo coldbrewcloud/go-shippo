499708059fe17f374b0caebc9fdc259eca467bb4
Daniel Kang <me@daniel.gs>
2016-09-27 20:17:17 -0700

    add "async" field to refund request

--- models/refund.go
+++ models/refund.go
@@ -3,6 +3,7 @@ package models
 // See https://goshippo.com/docs/reference#refunds
 type RefundInput struct {
 	Transaction string `json:"transaction"`
+	Async       bool   `json:"async"`
 }
 
 // See https://goshippo.com/docs/reference#refunds

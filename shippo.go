137690faae65142e1f83c6296dd4bacd0aa0766c
Derek Yu <yuderekyu@gmail.com>
2017-04-18 00:13:33 -0500

    Update import

--- shippo.go
+++ shippo.go
@@ -1,6 +1,6 @@
 package shippo
 
-import "github.com/coldbrewcloud/go-shippo/client"
+import "github.com/yuderekyu/go-shippo/client"
 
 func NewClient(privateToken string) *client.Client {
 	return client.NewClient(privateToken)
